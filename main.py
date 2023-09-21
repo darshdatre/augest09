ed071f958c82e79375954dcdd22e47b576c5519b
darshdatre <143878433+darshdatre@users.noreply.github.com>
2023-09-21 08:21:45 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+print("welcom python");
+a,b=10,20
+sum=a+b
+print("sum of two no is",sum)
+
+
+name ="darshan datre"
+# print(name)   #string
+print(name[1])
+
+
+emp= {1:"darshan",
+     2: "pavan",
+     3: "ram"}
+print(emp[2])
